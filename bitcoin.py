c0af1b24293e4c4a1a3d5d811d9932c903828b50
Aarch-code <58585532+Aarch-code@users.noreply.github.com>
2022-05-07 20:58:36 +0530

    Create bitcoin.py

---
+++ bitcoin.py
@@ -0,0 +1,24 @@
+"""
+    In this project, you'll create a program that that tells
+    you when the value of your Bitcoin falls below $30,000.
+
+    You will need to:
+    - Create a function to convert Bitcoin to USD
+    - If your Bitcoin falls below $30,000 print a message.
+
+    You can assume that 1 Bitcoin is worth $40,000
+
+"""
+
+investment_in_bitcoin = 1.2
+bitcoin_to_usd = 25000
+
+def bitcoinToUSD(investment_in_bitcoin, bitcoin_to_usd):
+  usd_value = investment_in_bitcoin * bitcoin_to_usd
+  return usd_value
+
+investment_in_usd = bitcoinToUSD(investment_in_bitcoin, bitcoin_to_usd)
+if investment_in_usd <= 30000:
+  print("Investment below $30,000! SELL!")
+else:
+  print("Investment above $30,000")
